74855d1aa4167fed711559982e4cdd5eed6a6e29
tamberg <tamberg@yaler.net>
2021-03-21 17:24:43 +0100

    Update.

--- OpenMV/OpenMVH7Plus_PirWakeUpPhotoLogging.py
+++ OpenMV/OpenMVH7Plus_PirWakeUpPhotoLogging.py
@@ -10,26 +10,24 @@ from pyb import Pin, ExtInt, LED
 
 PIR_PIN = Pin.board.P4
 RED_LED_ID = 1
-BLUE_LED_ID = 3
 
 def callback(line):
     pass
 
-red_led = LED(RED_LED_ID)
-blue_led = LED(BLUE_LED_ID)
+led = LED(RED_LED_ID)
 pir = Pin(PIR_PIN, Pin.IN)
 ext = ExtInt(pir, ExtInt.IRQ_RISING, Pin.PULL_NONE, callback)
 
-red_led.on()
+led.on()
 rtc = pyb.RTC()
 rtc.datetime((2021, 3, 21, 7, 16, 53, 0, 0)) # When connecting LiPo
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.WQXGA2)
 sensor.skip_frames(time = 2000) # Let settings take effect
-red_led.off()
+led.off()
 while True:
-    blue_led.on()
+    led.on()
     t = rtc.datetime()
     y = str(t[0])
     m = '%02d' % t[1]
@@ -40,5 +38,5 @@ while True:
     s = '%02d' % t[6]
     name = 'snapshot_' + y + '-' + m + '-' + d + 'T' + h + '-' + n + '-' + s + 'Z.jpg'
     sensor.snapshot().save(name) # 0.25 A
-    blue_led.off()
-    machine.sleep() # until external interrupt, 0.06 A
+    led.off()
+    machine.sleep() # until external interrupt triggered by PIR, 0.06 A
