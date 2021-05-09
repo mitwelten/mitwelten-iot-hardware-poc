661ec0292c065fc9a04159c511755ccdf9ce65f9
Thomas Amberg <tamberg@yaler.net>
2021-05-09 16:25:31 +0200

    Update convert.c

--- Posix/Convert/convert.c
+++ Posix/Convert/convert.c
@@ -9,16 +9,6 @@
 #include <string.h>
 #include <time.h>
 
-char *type_to_string(unsigned char type) {
-    char *result;
-    switch (type) {
-        case DT_DIR: result = "DT_DIR"; break;
-        case DT_REG: result = "DT_REG"; break;
-        default: result = "other";
-    }
-    return result;
-}
-
 char *get_extension(char *name) {
     char *p = NULL;
     while(*name != '\0') {
@@ -30,10 +20,20 @@ char *get_extension(char *name) {
     return p;
 }
 
-// TODO: conv_file_name, conv_dir_name
-int convert_name(char *name, char *conv, size_t conv_len) {
-    assert(conv != NULL);
-    assert(conv_len >= 0);
+// $ stat 20210209_173300T.WAV
+// "Feb 11 14:47:48 2021"
+// "Feb  9 17:33:54 2021"
+// "May  9 14:36:32 2021"
+// "Feb  9 17:32:58 2021"
+
+int convert_name(char *name,
+    char *conv_name, size_t conv_name_len,
+    char *conv_dir, size_t conv_dir_len) 
+{
+    assert(conv_name != NULL);
+    assert(conv_name_len >= 0);
+    assert(conv_dir != NULL);
+    assert(conv_dir_len >= 0);
     int result;
     char *ext = get_extension(name);
     if (ext != NULL &&
@@ -45,17 +45,22 @@ int convert_name(char *name, char *conv, size_t conv_len) {
         struct stat s;
         stat(name, &s);
         struct tm *t = gmtime(&s.st_mtime);
-        printf("%s\n", asctime(t)); // TODO: wrong date
-        // file name compatible ISO 8601, yyyy-mm-ddThh-mm-ssZ
-        const char *format = "%Y-%m-%dT%H-%M-%SZ"; // => 20
+        // ISO8601, replace ':' with '-'
+        // yyyy-mm-ddThh-mm-ssZ => 20 characters
         size_t len = 20 + 1; // '\0' terminated
-        assert(len + strlen(ext) <= conv_len);
-        size_t len2 = strftime(conv, len, format, t);
-        conv[len2] = '\0';
-        strcpy(conv + len2, ext); // including '\0'
+        assert(len + strlen(ext) <= conv_name_len);
+        len = strftime(conv_name, len, "%Y-%m-%dT%H-%M-%SZ", t);
+        conv_name[len] = '\0';
+        strcpy(conv_name + len, ext); // including '\0'
+        // yyyy-mm-dd => 20 characters
+        size_t len2 = 10 + 1; // '\0' terminated
+        assert(len2 <= conv_dir_len);
+        len2 = strftime(conv_dir, 10 + 1, "%Y-%m-%d", t);
+        conv_dir[len2] = '\0';
         result = 0;
     } else {
-        conv[0] = '\0';
+        conv_name[0] = '\0';
+        conv_dir[0] = '\0';
         result = -1;
     }
     return result;
@@ -63,22 +68,22 @@ int convert_name(char *name, char *conv, size_t conv_len) {
 
 int main() {
     // TODO: add file name prefix = enclosing directory name
-    int iso_name_max_len = 32;
-    char iso_name[iso_name_max_len];
+    int conv_str_max = 32;
+    char conv_name[conv_str_max];
+    char conv_dir[conv_str_max];
     DIR *d = opendir(".");
     struct dirent *e = readdir(d);
     while (e != NULL) {
-        if (e->d_type == DT_REG) {
-            int result = convert_name(e->d_name, iso_name, iso_name_max_len);
+        if (e->d_type == DT_REG) { // regular file
+            int result = convert_name(e->d_name, 
+                conv_name, conv_str_max, 
+                conv_dir, conv_str_max);
             if (result == 0) {
+                printf("%s => %s/%s\n", e->d_name, conv_dir, conv_name);
                 // TODO: mkdir conv_dir_name
                 // TODO: mv name conv_dir_name/conv_name
-                printf("%s, %s, %s, SUCCESS\n", e->d_name, type_to_string(e->d_type), iso_name);
-            } else {
-                //printf("%s, %s, SKIPPED\n", e->d_name, type_to_string(e->d_type));
+                // TODO: if mv fails, add post-fix to prevent overwriting files
             }
-        } else {
-            //printf("%s, %s, SKIPPED\n", e->d_name, type_to_string(e->d_type));
         }
         e = readdir(d);
     }
