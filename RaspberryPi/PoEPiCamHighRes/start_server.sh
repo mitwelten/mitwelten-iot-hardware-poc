8229cbae60a8ef2005929bdee50c5dfaf4e0949b
WullT <38081583+WullT@users.noreply.github.com>
2022-06-02 11:28:08 +0200

    Update start_server.sh

--- RaspberryPi/PoEPiCamHighRes/start_server.sh
+++ RaspberryPi/PoEPiCamHighRes/start_server.sh
@@ -3,6 +3,8 @@ QUALITY=90
 PORT=8080
 USERNAME=MY_USER
 PASSWORD=MY_PASSWORD
+WIDTH=4656
+HEIGHT=3496
 
 cd /home/pi/staticserver
-python3 server.py --port $PORT --username $USERNAME --password $PASSWORD --quality $QUALITY
+python3 server.py --port $PORT --username $USERNAME --password $PASSWORD --quality $QUALITY --width $WIDTH --height $HEIGHT
